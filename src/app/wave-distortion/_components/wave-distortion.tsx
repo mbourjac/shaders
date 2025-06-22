944c7bd9513b8f3d052b86958b59a7dd88b19256
Michaël Bourjac <michael.bourjac@gmail.com>
2025-06-22 17:34:51 +0200

    feat(wave-distortion): set up shader material

--- src/app/wave-distortion/_components/wave-distortion.tsx
+++ src/app/wave-distortion/_components/wave-distortion.tsx
@@ -1,8 +1,15 @@
+import fragmentShader from '../_shaders/fragment.glsl';
+import vertexShader from '../_shaders/vertex.glsl';
+
 export const WaveDistortion = () => {
   return (
     <mesh>
       <planeGeometry args={[3, 3, 45, 45]} />
-      <meshBasicMaterial color="red" />
+      <shaderMaterial
+        wireframe
+        vertexShader={vertexShader}
+        fragmentShader={fragmentShader}
+      />
     </mesh>
   );
 };
