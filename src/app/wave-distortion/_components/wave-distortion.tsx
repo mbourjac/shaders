4b9fa0880060ac9e694aa062383964ff1802485f
Michaël Bourjac <michael.bourjac@gmail.com>
2025-06-22 19:15:52 +0200

    feat(wave-distortion): animate wave

--- src/app/wave-distortion/_components/wave-distortion.tsx
+++ src/app/wave-distortion/_components/wave-distortion.tsx
@@ -6,20 +6,27 @@ import fragmentShader from '../_shaders/fragment.glsl';
 import vertexShader from '../_shaders/vertex.glsl';
 
 export const WaveDistortion = () => {
-  const { amplitude, waveLength } = useControls({
+  const { speed, amplitude, waveLength } = useControls({
+    speed: { value: 2, min: 0, max: 10, step: 0.5 },
     amplitude: { value: 0.25, min: 0, max: 5, step: 0.05 },
     waveLength: { value: 5, min: 0, max: 20, step: 1 },
   });
 
-  const planeRef = useRef<Mesh<PlaneGeometry, ShaderMaterial>>(null!);
+  const planeRef = useRef<Mesh<PlaneGeometry, ShaderMaterial>>(null);
   const uniformsRef = useRef({
+    uTime: { value: 0 },
     uAmplitude: { value: amplitude },
     uWaveLength: { value: waveLength },
   });
 
-  useFrame(() => {
-    planeRef.current.material.uniforms.uAmplitude.value = amplitude;
-    planeRef.current.material.uniforms.uWaveLength.value = waveLength;
+  useFrame((_, delta) => {
+    const plane = planeRef.current;
+
+    if (!plane) return;
+
+    plane.material.uniforms.uTime.value += delta * speed;
+    plane.material.uniforms.uAmplitude.value = amplitude;
+    plane.material.uniforms.uWaveLength.value = waveLength;
   });
 
   return (
--- src/app/wave-distortion/_shaders/vertex.glsl
+++ src/app/wave-distortion/_shaders/vertex.glsl
@@ -1,10 +1,11 @@
+uniform float uTime;
 uniform float uAmplitude;
 uniform float uWaveLength;
 
 void main() {
   vec3 newPosition = position;
   
-  float wave = uAmplitude * sin(position.x * uWaveLength);
+  float wave = uAmplitude * sin(position.x * uWaveLength + uTime);
   newPosition.z += wave;
 
   gl_Position = projectionMatrix * modelViewMatrix * vec4(newPosition, 1.0);
